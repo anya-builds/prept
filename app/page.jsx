fcaa310b67e50896330ec5426d53b11beaa9b467
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-04-26 11:57:14 +0530

    AI interview marketplace

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Button variant="gold" size="hero">Submit</Button>
+      <section className="pt-28 sm:pt-32">
+          <Button variant="gold" size="hero">Submit</Button>
+      </section>
     </div>
     
   );
